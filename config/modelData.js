7dfc6357a4b8cadd305f912c7fc90bf9985d21c3
Michael Kilgore <dauvis@hotmail.com>
2025-01-03 17:38:38 -0500

    Added endpoint for available models

---
+++ config/modelData.js
@@ -0,0 +1,12 @@
+const availableModels = [
+    {
+        id: "gpt-4o-mini",
+        name: "GPT 4o mini",
+    },
+    {
+        id: "gpt-4o",
+        name: "GPT 4o"
+    }
+];
+
+export default availableModels;
--- routes/apiOptionRoutes.js
+++ routes/apiOptionRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import systemMessageService from "../services/systemMessageService.js";
+import availableModels from "../config/modelData.js";
 
 const router = express.Router();
 
@@ -8,6 +9,16 @@ router.get("/api/v1/options/tones", authMiddleware, (req, res) => {
   const toneOptions = systemMessageService.getToneOptionList();
   res.json({
     options: toneOptions
+  });
+});
+
+router.get("/api/v1/options/models", authMiddleware, (req, res) => {
+  const modelOptions = availableModels.map(entry => {
+    return { value: entry.id, label: entry.name};
+  });
+
+  res.json({
+    options: modelOptions
   });
 });
 
