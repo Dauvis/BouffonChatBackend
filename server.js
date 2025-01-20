e0fc9a08f510f0df2ea16139edfd8994dec4ab0c
Michael Kilgore <dauvis@hotmail.com>
2025-01-20 15:36:58 -0500

    hosting changes

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ const app = express();
 app.use(cors({ origin: config.webAppSite, credentials: true }));
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again later.'
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.',
+    validate: {xForwardedForHeader: false}
 });
 
 app.use(limiter);
